97704c6d19b2983cb975cd2de7e41a2791b1e64b
Baker Quillin <bakerquil@gmail.com>
2021-01-21 14:15:37 -0500

    changed app.js to use a router, started a login form component that is pulling into the signin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
-class App extends Component {
-  render() {
+import "./App.css";
+import Signup from "./pages/signup"
+
+const  App = () => {
+  
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+      <Route exact path="/" component={Signup} />
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+      </Router>
     );
   }
-}
+
 
 export default App;
