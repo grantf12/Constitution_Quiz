efe136935c798c8d32557a24fafe511593218a7c
Grant <grantferment@gmail.com>
2021-01-21 13:48:39 -0500

    corrected the comment for mongoose/MongoDB

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -16,7 +16,10 @@ app.get("*", function(req, res) {
 });
 
 // Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/constitutionproject");
+// Change to this one below when everything is ready to go online
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/constitutionproject");
+
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
