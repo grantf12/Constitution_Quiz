897300c953cd32f1b48641d050fd2aa73aca0c39
Paul Carr <pwcarr15@gmail.com>
2021-01-22 10:31:30 -0500

    Changed and added questions to be constitutional based.

--- client/src/pages/quiz.js
+++ client/src/pages/quiz.js
@@ -3,21 +3,89 @@ import React, {useState} from "react";
 const quiz = () => {
     const questions = [
         {
-            question: "What is JavaScript?",
+            question: "When was the US Constitution signed?",
             choices: [
-                { option: "coffee", correct: false},
-                { option: "a potentially brutal programming language", correct: true},
-                { option: "religious text", correct: false},
-                {option: "a type of paper", correct: false}
+                { option: "June 22, 1788", correct: false},
+                { option: "September 17, 1787", correct: true},
+                { option: "July 4, 1776", correct: false},
+                {option: "March 4, 1789", correct: false}
             ]
         },
         {
-            question: "Variables in JavaScript can contain what?",
+            question: "How many articles are in the US Constitution?",
             choices: [
-                { option: "string only", correct: false},
-                { option: "nothing", correct: false},
-                { option: "numbers only", correct: false},
-                {option: "all of the above", correct: true}
+                { option: "10", correct: false},
+                { option: "7", correct: true},
+                { option: "2", correct: false},
+                {option: "15", correct: false}
+            ]
+        },
+        {
+            question: "How many amendments does the US Constitution have?",
+            choices: [
+                { option: "27", correct: true},
+                { option: "10", correct: false},
+                { option: "32", correct: false},
+                {option: "50", correct: false}
+            ]
+        },
+        {
+            question: "Does the constitution have spelling errors?",
+            choices: [
+                { option: "True", correct: true},
+                { option: "False", correct: false},
+            ]
+        },
+        {
+            question: "The constitution had a bill of rights when it was ratified.",
+            choices: [
+                { option: "True", correct: false},
+                { option: "False", correct: true},
+            ]
+        },
+        {
+            question: "About how long did it take to write the constitution?",
+            choices: [
+                { option: "10 years", correct: false},
+                { option: "2 weeks", correct: false},
+                { option: "4 months", correct: true},
+                {option: "1 year", correct: false}
+            ]
+        },
+        {
+            question: "Who did not help write the constitution?",
+            choices: [
+                { option: "Benjamin Franklin", correct: false},
+                { option: "Alexander Hamilton", correct: false},
+                { option: "James Madison", correct: false},
+                {option: "Thomas Jefferson", correct: true}
+            ]
+        },
+        {
+            question: "How many words are in the constitution?",
+            choices: [
+                { option: "10,000", correct: false},
+                { option: "2,360", correct: false},
+                { option: "4,543", correct: true},
+                {option: "5,201", correct: false}
+            ]
+        },
+        {
+            question: "Which amendment to the constitution was repealed?",
+            choices: [
+                { option: "2nd amendment", correct: false},
+                { option: "18th amendment", correct: true},
+                { option: "10th amendment", correct: false},
+                {option: "14th amendment", correct: false}
+            ]
+        },
+        {
+            question: "When was the constitution last amended?",
+            choices: [
+                { option: "October 7, 1789", correct: false},
+                { option: "January 20, 1852", correct: false},
+                { option: "July 4, 1910", correct: false},
+                {option: "May 5, 1992", correct: true}
             ]
         }
     ];
