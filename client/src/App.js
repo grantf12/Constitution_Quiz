9d86aa4b583ad8f56f0aab795b418e327840a66a
Baker Quillin <bakerquil@gmail.com>
2021-01-22 11:11:50 -0500

    adding state to the app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,38 @@ import "./App.css";
 import Signup from "./pages/signup";
 import Home from "./pages/home";
 
-const App = () => {
-  
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      loggedIn: false,
+      username: null
+    }
+    this.getUser = this.getUser.bind(this)
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.updateUser = this.updateUser.bind(this)
+  }
+  componentDidMount() {
+    this.getUser()
+  }
+  updateUser (userObject) {
+    this.setState(userObject)
+  }
+  getUser() {
+    axios.get("/user/").then(response => {
+      if (response.data.user) {
+        this.setState({
+          loggedIn: true,
+          username: response.data.user.username
+        })
+      } else {
+        this.setState({
+          loggedIn: false, 
+          username: null
+        });
+      }
+    })
+  }
   render(){
     return (
       <Router>
